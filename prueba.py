0376bb4686614d8bca5491388786c9d0b94a95d2
maxiiol <134154147+maxiiol@users.noreply.github.com>
2023-12-19 16:16:07 -0300

    Update prueba.py

---
+++ prueba.py
@@ -0,0 +1,16 @@
+class RECTANGULO:
+    def __init__(self,ancho,longitud) :
+        self.ancho=ancho
+        self.longitud=longitud
+
+    def CalculoPerimetro(self):
+        return 2 * self.ancho + 2*self.longitud
+            
+    def CalculoArea (self):
+        return self.ancho * self.longitud
+    
+objeto1 = RECTANGULO(20,15)
+
+
+print("El perímetro del rectángulo es:", objeto1.CalculoPerimetro())
+print("El área del rectángulo es:", objeto1.CalculoArea())
