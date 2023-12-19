c234d5c70d103079e3b4f64a1eef38d31be00bf8
joshuareynoso <134154349+joshuareynoso@users.noreply.github.com>
2023-12-19 16:27:41 -0300

    1
    algo

--- REPASO.py
+++ REPASO.py
@@ -3,7 +3,13 @@ class Rectangulo:
         self.ancho=ancho
         self.largo=largo    
         
+    def calculo(self):
+        perimetro = 2 * self.ancho + 2*self.longitud
+        return perimetro
     
+    def calculo2 (self):
+        area = self.ancho * self.longitud
+        return area
     
     
     
